3a4179eafdbb14652d78a4e1e6c6ff305628e5c2
vitosotdihaet <vitostikitos@gmail.com>
2024-12-31 04:48:01 +0300

    Start implementing websocket connection

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,9 @@
-use std::{collections::HashMap, fs, net::SocketAddr, path::Path, str::FromStr};
-use warp::Filter;
+use std::{collections::HashMap, net::SocketAddr, str::FromStr, sync::Arc};
+
+use futures_util::{stream::StreamExt, SinkExt, TryFutureExt};
+use tokio::sync::{mpsc, RwLock};
+use tokio_stream::wrappers::UnboundedReceiverStream;
+use warp::{filters::ws::{Message, WebSocket}, Filter};
 
 
 #[macro_use]
@@ -12,18 +16,15 @@ struct ServerProperties {
 }
 
 fn get_server_properties() -> Result<ServerProperties, Box<dyn std::error::Error>> {
-    let ip = dotenv!("HOST");
-    let port= dotenv!("PORT");
-
     env_logger::builder()
         .filter_level(log::LevelFilter::from_str(dotenv!("RUST_LOG"))?)
-        .format_timestamp(None)
-        .format_target(false)
         .init();
 
+    let host = dotenv!("HOST");
+    let port= dotenv!("PORT");
 
-    let address: SocketAddr = format!("{ip}:{port}").parse::<SocketAddr>().unwrap_or_else(move |err| {
-        eprintln!("error: {err}: {ip}:{port}");
+    let address: SocketAddr = format!("{host}:{port}").parse::<SocketAddr>().unwrap_or_else(move |err| {
+        log::error!("{err} : HOST={host} PORT={port}");
         "127.0.0.1:8000".parse().unwrap()
     });
 
@@ -32,22 +33,63 @@ fn get_server_properties() -> Result<ServerProperties, Box<dyn std::error::Error
     })
 }
 
+/// TODO
+///
+/// key is a game "id"
+type Games = Arc<RwLock<HashMap<usize, mpsc::UnboundedSender<Message>>>>;
+
+async fn player_connected(ws: WebSocket, games: Games, game_id: u64) {
+    log::info!("ws={ws:?}, games={games:?}, game_id={game_id}");
+
+    let (mut player_ws_tx, mut player_ws_rx) = ws.split();
+
+    // Use an unbounded channel to handle buffering and flushing of messages
+    // to the websocket...
+    let (tx, rx) = mpsc::unbounded_channel();
+    let mut rx = UnboundedReceiverStream::new(rx);
+
+    tokio::task::spawn(async move {
+        while let Some(message) = rx.next().await {
+            player_ws_tx
+                .send(message)
+                .unwrap_or_else(|e| {
+                    log::error!("websocket send error: {}", e);
+                })
+                .await;
+        }
+    });
+}
+
 
 #[tokio::main]
 async fn main() {
     let server_properties = get_server_properties().expect("Could not set up the server");
-    
+
+    let games = Games::default();
+    let games = warp::any().map(move || games.clone());
+
+    let enter_game = warp::path("game")
+        // The `ws()` filter will prepare Websocket handshake...
+        .and(warp::ws())
+        .and(games)
+        .map(|ws: warp::ws::Ws, games| {
+            // This will call our function if the handshake succeeds.
+            log::info!("hi?");
+            ws.on_upgrade(move |socket| player_connected(socket, games, 123))
+        });
+
     let root = warp::fs::dir("../frontend/dist")
-        .or(warp::fs::file("../frontend/dist/index.html"));
+        .or(warp::fs::file("../frontend/dist/index.html"))
+        .or(enter_game);
 
     let (_, server) = warp::serve(root)
         .bind_with_graceful_shutdown(server_properties.address, async move {
             tokio::signal::ctrl_c()
                 .await
-                .expect("failed to listen to shutdown signal");
+                .expect("failed to listen to a shutdown signal");
         });
-    
+
+    log::info!("Server is running at {}", server_properties.address);
     server.await;
-    
     log::info!("Server was shut down");
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,11 +6,32 @@ import App from './components/App';
 import './reset.css';
 import './styles.css';
 
-import init, { greet } from '../wasm-lib';
+// import init, { greet } from '../wasm-lib';
 
-init().then(() => {
-    greet();
-});
+// init().then(() => {
+//     greet();
+// });
+
+// TODO: Add websocket support
+const url = 'ws://' + location.host + '/game';
+const ws = new WebSocket(url);
+console.log(ws);
+
+function message(data) {
+    console.log(data);
+}
+
+ws.onopen = function () {
+    console.log('Player connected!');
+};
+
+ws.onmessage = function (msg) {
+    message(msg.data);
+};
+
+ws.onclose = function () {
+    console.log('Player left!');
+};
 
 const root = createRoot(document.querySelector('#root'));
 root.render(<App />);
